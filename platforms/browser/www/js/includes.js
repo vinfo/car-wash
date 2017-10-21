3c2d304ad656421be547261b9feac85c03b8f3ab
VICTOR VALENCIA <victor.valencia@gmail.com>
2017-10-21 17:14:42 -0500

    Changes2011021V2

--- platforms/browser/www/js/includes.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(function(){
-/*  $( ".page-header" ).load( "includes/page-header.html", function() {
-  	$("..rd-navbar-fixed").css("display","block");
-  });*/
-
-  $( ".back" ).click(function() {
-    window.history.back();
-  });     
-});
