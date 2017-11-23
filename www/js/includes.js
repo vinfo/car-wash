ddc43cd2730d8e3a0cf9d4fabde24749e244301b
VICTOR VALENCIA <victor.valencia@gmail.com>
2017-11-23 15:14:38 -0500

    Changes20171123

--- www/js/includes.js
+++ www/js/includes.js
@@ -21,3 +21,14 @@ var getUrlParameter = function getUrlParameter(sParam) {
         }
     }
 };
+$('input[type="date"], input[type="datetime"], input[type="datetime-local"], input[type="month"], input[type="time"], input[type="week"]').each(function() {
+    var el = this, type = $(el).attr('type');
+    if ($(el).val() == '') $(el).attr('type', 'text');
+    $(el).focus(function() {
+        $(el).attr('type', type);
+        el.click();
+    });
+    $(el).blur(function() {
+        if ($(el).val() == '') $(el).attr('type', 'text');
+    });
+});
